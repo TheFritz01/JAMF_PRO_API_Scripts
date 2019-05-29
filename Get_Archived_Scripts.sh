bb6da6c995407375d3c74d549991df8ccbc09795
TheFritz01 <TheFritz01@users.noreply.github.com>
2019-05-29 12:36:30 -0500

    Update Get_Archived_Scripts.sh

--- Get_Archived_Scripts.sh
+++ Get_Archived_Scripts.sh
@@ -22,7 +22,7 @@
 API_AUTH=$(echo `echo <Base64 Encoded Password> | base64 --decode`) #Do not include < > around your encoded password!
 JSSURL="https://jss.yourserver.com"
 
-## Archive Category to check (Default is zArchived Scripts" but you can change this to one of your choosing)
+## Archive Category to check (Default is "zArchived Scripts" but you can change this to one of your choosing)
 ARCHIVE="zArchived Scripts"
 
 ##### Functions #####
